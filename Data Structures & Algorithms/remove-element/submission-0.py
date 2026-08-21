0cc28d4cb6172345591680f2e16ee258beb9bfb2
Steven Chen <stevenchen6906@gmail.com>
2026-08-20 23:15:50 -0400

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0 # pointer for where to place kept elements
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i] # keep this element
+                k += 1
+        return k
+
+            
+
+
+        # walk through every element in the array using one pointer
+        # annother pointer would track where the next "kept" element would go
+        # if we find an element that isn't val, we would copy it to position k and then advance to k
+        # if not val then just skip
